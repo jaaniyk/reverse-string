87e5a426226e9e6716bf111bf34a57636a2e8719
Yuvraj Karanjikar <61325298+jaaniyk@users.noreply.github.com>
2020-02-24 00:14:45 +0530

    Add files via upload

--- Q11.cpp
+++ Q11.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
-int factorial(int n){
+void factorial(){
+	int n;
+	cout<< "Enter the Number for Factorial :";
+	cin>>n;
 	cout<<"the factors of "<< n <<" is :";
 	int fact=1;
 	for(int i=1;i<=n;i++ ){
 		fact=fact*i;
 	}
-	return fact;
+	cout<<fact;
 }
 int main(){
-	int f;
-	cout<< "Enter the Number for Factorial :";
-	cin>>f;
-  	int NoFact=factorial(f);
-	  cout<<NoFact<<endl;
-	  return 0;	
+
+	factorial();
+	return 0;	
 }
 
